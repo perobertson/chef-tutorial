22fc232502f82c9d8ee26f09b061fdf84897a8e4
Paul Robertson <t.paulrobertson@gmail.com>
2016-02-09 12:52:06 -0500

    added encrypted data bags

--- cookbooks/awesome_customers/attributes/default.rb
+++ cookbooks/awesome_customers/attributes/default.rb
@@ -4,3 +4,5 @@ default['awesome_customers']['group'] = 'web_admin'
 default['awesome_customers']['document_root'] = '/var/www/customers/public_html'
 
 default['awesome_customers']['enabled_firewall_rules'] = %w(firewall_http firewall_sshd)
+
+default['awesome_customers']['passwords']['secret_path'] = '/etc/chef/encrypted_data_bag_secret'
